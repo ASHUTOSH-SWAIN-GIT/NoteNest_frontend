34426c4e5469c8a51dd262c59354e2796431bbdf
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-04-26 21:16:06 +0530

    deployment

--- app/sign-up/components/signup-form-demo.tsx
+++ app/sign-up/components/signup-form-demo.tsx
@@ -1,9 +1,7 @@
 "use client"
 import type React from "react"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
 import { cn } from "@/lib/utils"
-import { IconBrandGithub, IconBrandGoogle, IconBrandOnlyfans } from "@tabler/icons-react"
+import {  IconBrandGoogle } from "@tabler/icons-react"
 import { supabase } from "@/lib/supabaseClient"; // Adjust import as per your setup
 
 export default function SignupFormDemo() {
